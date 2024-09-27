547e2095f63090088de2611a9f2f7c447b5d9ea3
Burak <burak.yn.dev@gmail.com>
2024-09-28 01:37:07 +0300

    🗃️ Update database schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { boolean, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("user", {
     id: text("id").primaryKey(),
@@ -12,7 +12,31 @@ export const sessionTable = pgTable("session", {
     expiresAt: timestamp("expires_at", {withTimezone: true, mode: "date"}).notNull()
 })
 
+export const channelsTable = pgTable("channel", {
+    id: text("id").primaryKey(),
+    owner: text("owner_id").notNull().references(()=>userTable.id),
+    participants: text("participant_ids").references(()=>userTable.id).array(),
+    name: varchar("name", {length: 120}),
+    creationTime: timestamp("created", {withTimezone: true, mode: "date"}).notNull(),
+    isDirectMessage: boolean("is_dm")
+})
+
+export const messagesTable = pgTable("message", {
+    id: text("id").primaryKey(),
+    channelId: text("channel_id").references(()=>channelsTable.id),
+    sender: text("sender_id").references(()=>userTable.id),
+    content: text("content").notNull(),
+    sentAt: timestamp("sent_at").notNull()
+})
+
 export type User = typeof userTable.$inferSelect;
 export type NewUser = typeof userTable.$inferInsert;
+
 export type Session = typeof sessionTable.$inferSelect;
 export type NewSession = typeof sessionTable.$inferInsert;
+
+export type Channel = typeof channelsTable.$inferSelect;
+export type NewChannel = typeof channelsTable.$inferInsert;
+
+export type Message = typeof messagesTable.$inferSelect;
+export type NewMessage = typeof messagesTable.$inferInsert;
